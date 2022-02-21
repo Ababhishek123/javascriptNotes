6dba271a9cf52755b8838e816dd68096be9cc763
Ababhishek123 <ababhishek7397@gmail.com>
2022-02-21 14:21:57 +0530

    first commit

---
+++ notes.js
@@ -0,0 +1,29 @@
+    //comments
+// inline comments 
+/* multi line comments
+in javascript */
+
+
+    //data types in javascript
+
+
+//declaring a variabla
+
+// var naam="abhishek";
+// console.log(naam);
+
+// var a= 33/2;
+// console.log(a);
+ 
+// var b= 33%2;
+// console.log(b);
+
+// console.log("i don\'t know")    //i don't know
+// console.log("hii \"double quotes\"") //hii "double quotes"
+// console.log("this is \\ backslash")
+// console.log("this is \n start of new line")
+console.log("hii \r  hfh carriage return \r helloooo multiverse")
+
+console.log("helllo \t \t \t multiverse")
+
+console.log("hello \b \b \b\b  backspace")
